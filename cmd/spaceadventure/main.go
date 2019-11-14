42d7f1115bd0d4712f7ff0d1a3887c19fdde4fec
clay-town <justinmccune@icloud.com>
2019-11-13 20:22:48 -0800

    Update main to accept Planets struct

--- cmd/spaceadventure/main.go
+++ cmd/spaceadventure/main.go
@@ -3,5 +3,11 @@ package main
 import "github.com/clay-town/go-spaceadventure-walkthrough/internal/spaceadventure"
 
 func main() {
-	spaceadventure.Start(spaceadventure.PlanetarySystem{"Solar System"})
+	spaceadventure.Start(
+		spaceadventure.PlanetarySystem{
+			Name: "Solar System", Planets: []spaceadventure.Planet{
+				spaceadventure.Planet{Name: "Arrakis", Description: "A desert planet, it's only resource is Melange."},
+			},
+		},
+	)
 }
