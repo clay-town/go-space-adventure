5cbc7d121c0a44d1f6af00fb0e2b5f9dc2afde97
clay-town <justinmccune@icloud.com>
2019-11-13 20:25:02 -0800

    Implement func that returns number of Planets

--- internal/spaceadventure/planetarysystem.go
+++ internal/spaceadventure/planetarysystem.go
@@ -5,3 +5,8 @@ type PlanetarySystem struct {
 	Name    string
 	Planets []Planet
 }
+
+//NumberOfPlanets returns the length of the Planets struct
+func (ps PlanetarySystem) NumberOfPlanets() int {
+	return len(ps.Planets)
+}
